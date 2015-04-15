6459d57d1515e60e3e6ad15376959c83b3288186
Alex <alexandralaudon@gmail.com>
2015-04-15 16:59:57 -0600

    Added Registrations

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You have now registered to write a message for Jeffery.  Please proceed below"
+      redirect_to root_path
+    else
+      flash[:notice] = "Asshole.  Please don\'t mess up the system.  I\'m trying to do something"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image_url, :message)
+  end
+
+end
