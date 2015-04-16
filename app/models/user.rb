0a426ce85abcf316c8c3e91712e9855b7cf50dba
Alex <alexandralaudon@gmail.com>
2015-04-15 19:32:30 -0600

    Trying to adjust the margin between images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  validates_presence_of :name, :message 
   has_attached_file :avatar, :styles => { :medium => "250x250>", :thumb => "200x200>" }, :default_url => "/images/:style/missing.png"
   validates_attachment :avatar,
   :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
