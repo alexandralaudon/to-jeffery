650ae6d90f853484d2c55874027f3829074469b6
Alex <alexandralaudon@gmail.com>
2015-04-15 18:32:12 -0600

    Updated Paperclip for cleaner images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
-  validates :avatar, :attachment_presence => true
-  validates_with AttachmentPresenceValidator, :attributes => :avatar
-  validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "200x200>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment :avatar,
+  :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
 
 end
