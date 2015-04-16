d987929194a925481466ffb321375269ff23273e
Alex <alexandralaudon@gmail.com>
2015-04-15 18:08:22 -0600

    Updated image avatar to have validations around it

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,10 +7,9 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "You have now registered to write a message for Jeffery.  Please proceed below"
       redirect_to root_path
     else
-      flash[:notice] = "Asshole.  Please don\'t mess up the system.  I\'m trying to do something"
+      flash[:notice] = "PLEASE DON'T MESS UP THE SYSTEM."
       render :new
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  validates :avatar, :attachment_presence => true
+validates_with AttachmentPresenceValidator, :attributes => :avatar
+validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
+
 end
